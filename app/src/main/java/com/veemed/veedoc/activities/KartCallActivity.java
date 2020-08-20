8f97cc1380477e8c8ca97346183e94a5a6430a02
naveed-iqbal <h.naveediqbal@gmail.com>
2020-08-20 07:20:25 +0500

    Added gestures sample code in and incall features buttons in KartCall Activity

--- app/src/main/java/com/veemed/veedoc/activities/KartCallActivity.java
+++ app/src/main/java/com/veemed/veedoc/activities/KartCallActivity.java
@@ -8,8 +8,10 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.opentok.android.OpentokError;
 import com.opentok.android.Publisher;
@@ -29,7 +31,7 @@ import retrofit2.Call;
 import retrofit2.Response;
 
 public class KartCallActivity extends AppCompatActivity implements Session.SessionListener,
-        PublisherKit.PublisherListener, View.OnTouchListener {
+        PublisherKit.PublisherListener, View.OnTouchListener, View.OnClickListener {
 
     private static final String TAG = KartCallActivity.class.getSimpleName();
     private static String API_KEY = "";
@@ -40,11 +42,22 @@ public class KartCallActivity extends AppCompatActivity implements Session.Sessi
     private static final int RC_VIDEO_APP_PERM = 124;
     private Session mSession;
 
+    private ImageView ivCallCancel;
+    private ImageView ivMic;
+    private ImageView ivCamera;
+
     private Publisher mPublisher;
     private Subscriber mSubscriber;
 
     private FrameLayout mPublisherViewContainer;
     private FrameLayout mSubscriberViewContainer;
+
+    private ScaleGestureDetector mScaleDetector;
+    private float mScaleFactor = 1.f;
+
+    private boolean isAudioPublished = true;
+    private boolean isVideoPublished = true;
+    private float mLastScaleFactor;
 
     RetrofitCallbackListener<SessionInfo> sessionInfoCallbackListener = new RetrofitCallbackListener<SessionInfo>() {
         @Override
@@ -71,17 +84,21 @@ public class KartCallActivity extends AppCompatActivity implements Session.Sessi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kart_call);
 
+        ivCallCancel = findViewById(R.id.ivCallEnd);
+        ivCamera = findViewById(R.id.ivVideo);
+        ivMic = findViewById(R.id.ivMic);
+
+        ivCallCancel.setOnClickListener(this);
+        ivCamera.setOnClickListener(this);
+        ivMic.setOnClickListener(this);
+
         mPublisherViewContainer = (FrameLayout)findViewById(R.id.publisher_container);
         mSubscriberViewContainer = (FrameLayout)findViewById(R.id.subscriber_container);
 
         mSubscriberViewContainer.setOnTouchListener(this);
         VeeDocRepository userRepo = VeeDocRepository.getInstance();
 
-
         SessionInfo sessionInfo = (SessionInfo) getIntent().getSerializableExtra("session");
-
-
-
         if(sessionInfo == null) { // this means call requested from endpoints screen
             int id = getIntent().getIntExtra("sessionId", -1);
             if (id != -1) {
@@ -93,6 +110,8 @@ public class KartCallActivity extends AppCompatActivity implements Session.Sessi
         } else { // call accepted from currentSessions
             initCall(sessionInfo);
         }
+
+        mScaleDetector = new ScaleGestureDetector(this, new KartCallActivity.ScaleListener());
     }
 
     @Override
@@ -191,19 +210,71 @@ public class KartCallActivity extends AppCompatActivity implements Session.Sessi
 
     private void publishAudio(boolean bool) {
         mPublisher.setPublishAudio(bool);
-        mPublisher.setPublishVideo(bool);
+        if(bool) {
+            ivMic.setImageResource(R.drawable.ic_mic);
+        } else {
+            ivMic.setImageResource(R.drawable.ic_mic_off);
+        }
+        isAudioPublished = bool;
     }
 
     private void publishVideo(boolean bool) {
         mPublisher.setPublishVideo(bool);
+        if(bool) {
+            ivCamera.setImageResource(R.drawable.ic_video_cam);
+        } else {
+            ivCamera.setImageResource(R.drawable.ic_video_cam_off);
+        }
+        isVideoPublished = bool;
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
 
+        mScaleDetector.onTouchEvent(motionEvent);
         if(motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-
+            if(motionEvent.getPointerCount() == 1) {
+                // TODO send move signal
+            }
         }
-        return false;
+
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.ivCallEnd:
+                mSession.disconnect();
+                break;
+
+            case R.id.ivVideo:
+                publishVideo(!isVideoPublished);
+                break;
+
+            case R.id.ivMic:
+                publishAudio(!isAudioPublished);
+                break;
+        }
+    }
+
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+
+            // Don't let the object get too small or too large.
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+            if(mScaleFactor < mLastScaleFactor) {
+                // TODO send zoom signal
+            } else if(mScaleFactor > mLastScaleFactor) {
+                // TODO send zoom signal
+            }
+            mLastScaleFactor = mScaleFactor;
+            // invalidate();
+            return true;
+        }
     }
 }
